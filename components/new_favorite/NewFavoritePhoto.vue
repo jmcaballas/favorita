e636268242477e5c28a89390239a26dd98745cda
Joseph Matthew Caballas <joseph.caballas@bld.ai>
2023-08-17 02:55:09 +0800

    feat: Create NewFavoritePhoto component

---
+++ components/new_favorite/NewFavoritePhoto.vue
@@ -0,0 +1,9 @@
+<template>
+  <label class="label">
+    <span class="label-text">New Favorite Food</span>
+  </label>
+  <input
+    type="file"
+    class="file-input file-input-bordered w-full file-input-primary"
+  />
+</template>
--- pages/new-favorite.vue
+++ pages/new-favorite.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container mx-auto p-4 h-screen">
-    <NewFavoriteText />
+    <form class="form-control flex justify-center w-full">
+      <NewFavoriteText />
+      <NewFavoritePhoto />
+    </form>
   </div>
 </template>
