a65b42f4a30748fb3514ca9d10ca48cfc71d9603
Joseph Matthew Caballas <joseph.caballas@bld.ai>
2023-08-16 23:01:27 +0800

    Create header with dropdown menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
-    Some default layout shared across all pages
+    <header>
+      <nav class="flex bg-red-200">
+        <button @click="toggleDropdownMenu">
+          <div>Icon</div>
+        </button>
+        <NuxtLink to="/">Mi Comida Favorita</NuxtLink>
+      </nav>
+
+      <ul :class="{ hidden: !showDropdownMenu }">
+        <li>User</li>
+        <li>Tags</li>
+        <li>Theme</li>
+        <li>Logout</li>
+      </ul>
+    </header>
     <slot />
   </div>
 </template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const showDropdownMenu = useState("showDropdownMenu", () => false);
+
+const toggleDropdownMenu = () => {
+  showDropdownMenu.value = !showDropdownMenu.value;
+};
+
+watch(route, () => {
+  showDropdownMenu.value = false;
+});
+</script>
