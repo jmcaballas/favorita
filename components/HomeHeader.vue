7d977caa5820ca190364935a22dc614ffc56c903
Joseph Matthew Caballas <joseph.caballas@bld.ai>
2023-08-17 01:39:03 +0800

    feat: Create and style HomeHeader component

---
+++ components/HomeHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <div class="bg-primary">
+      <div class="container mx-auto p-4">
+        <nav class="flex justify-between">
+          <NuxtLink to="/">
+            <h1>Favorita</h1>
+          </NuxtLink>
+
+          <div class="dropdown dropdown-end">
+            <label tabindex="0">
+              <Icon name="ci:chevron-down" size="1.5em" />
+            </label>
+            <ul
+              tabindex="0"
+              class="dropdown-content menu z-[1] bg-secondary rounded-box w-52"
+            >
+              <li><a>User</a></li>
+              <li><a>Tags</a></li>
+              <li><a>Theme</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,15 @@
 <template>
-  <div>
-    <header>
-      <nav class="flex bg-red-200">
-        <button @click="toggleDropdownMenu">
-          <div>Icon</div>
-        </button>
-        <NuxtLink to="/">Mi Comida Favorita</NuxtLink>
-      </nav>
-
-      <ul :class="{ hidden: !showDropdownMenu }">
-        <li>User</li>
-        <li>Tags</li>
-        <li>Theme</li>
-        <li>Logout</li>
-      </ul>
-    </header>
+  <div class="divide-y">
+    <HomeHeader />
     <slot />
   </div>
 </template>
 
-<script setup lang="ts">
-import { useRoute } from "vue-router";
-
-const route = useRoute();
-
-const showDropdownMenu = useState("showDropdownMenu", () => false);
-
-const toggleDropdownMenu = () => {
-  showDropdownMenu.value = !showDropdownMenu.value;
-};
-
-watch(route, () => {
-  showDropdownMenu.value = false;
-});
-</script>
+<style>
+body {
+  font-family: Roboto;
+}
+h1 {
+  font-family: Libre Baskerville;
+}
+</style>
