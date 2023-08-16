904ca3f766a8e63d180b1edd029e49ee9a15a1c1
Joseph Matthew Caballas <joseph.caballas@bld.ai>
2023-08-17 02:14:28 +0800

    Create ADD NEW FAVORITE button

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="divide-y divide-dashed divide-primary">
-    <HomeHeader />
-    <slot />
-  </div>
+  <HomeHeader />
+  <slot />
 </template>
 
 <style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>index page</div>
+  <div class="container mx-auto p-4">
+    <div class="flex justify-center">
+      <NuxtLink
+        to="/new-favorite"
+        class="btn btn-sm sm:btn-sm md:btn-md lg:btn-lg btn-primary"
+      >
+        <Icon name="ci:plus-circle-outline" size="2em" />
+        ADD NEW FAVORITE
+      </NuxtLink>
+    </div>
+  </div>
 </template>
