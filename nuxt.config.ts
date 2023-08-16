a3e820b9b0369f78e456c392fd01beb073cf62e7
Joseph Matthew Caballas <joseph.caballas@bld.ai>
2023-08-16 23:56:45 +0800

    feat: Install Nuxt Google Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "nuxt-icon"],
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@nuxtjs/google-fonts",
+  ],
+  googleFonts: {
+    families: {
+      Roboto: true,
+      "Libre Baskerville": {
+        wght: [700],
+      },
+    },
+  },
 });
