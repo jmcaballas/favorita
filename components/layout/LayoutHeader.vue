81dedb227bd98ec00016476ee11e958d1a04b35f
Joseph Matthew Caballas <joseph.caballas@bld.ai>
2023-08-23 02:34:02 +0800

    fix: Toggle dropdown menu using icon

--- components/layout/LayoutHeader.vue
+++ components/layout/LayoutHeader.vue
@@ -7,8 +7,11 @@
             <h1>Favorita</h1>
           </NuxtLink>
 
-          <div class="dropdown dropdown-end">
-            <label tabindex="0">
+          <div
+            class="dropdown dropdown-end"
+            @focusout="handleDropdownFocusLost"
+          >
+            <label tabindex="0" @click="handleDropdownClick">
               <Icon
                 name="ci:chevron-down"
                 size="1.5em"
@@ -18,8 +21,17 @@
             <ul
               tabindex="0"
               class="dropdown-content menu z-[1] bg-base-200 rounded-box w-52"
+              v-show="isDropdownOpen"
             >
-              <li><a class="hover:bg-secondary-focus">User</a></li>
+              <li>
+                <NuxtLink
+                  to="/new-favorite"
+                  onclick="document.activeElement.blur()"
+                  class="hover:bg-secondary-focus"
+                >
+                  User
+                </NuxtLink>
+              </li>
               <li><a class="hover:bg-secondary-focus">Tags</a></li>
               <li><a class="hover:bg-secondary-focus">Theme</a></li>
               <li><a class="hover:bg-secondary-focus">Logout</a></li>
@@ -30,3 +42,20 @@
     </div>
   </header>
 </template>
+
+<script setup lang="ts">
+const isDropdownOpen = useState("isDropdownOpen", () => false);
+
+const handleDropdownClick = () => {
+  isDropdownOpen.value = !isDropdownOpen.value;
+};
+
+const handleDropdownFocusLost = ({ relatedTarget, currentTarget }) => {
+  if (
+    relatedTarget instanceof HTMLElement &&
+    currentTarget.contains(relatedTarget)
+  )
+    return;
+  isDropdownOpen.value = false;
+};
+</script>
