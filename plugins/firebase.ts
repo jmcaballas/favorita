5f5cbe71b69080b961538c0b805562f7e0086e23
Joseph Matthew Caballas <joseph.caballas@bld.ai>
2023-08-24 22:08:50 +0800

    fix: Fix Firebase config

---
+++ plugins/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { defineNuxtPlugin, useRuntimeConfig } from "nuxt/app";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
+
+  const firebaseConfig = {
+    apiKey: config.public.apiKey as string,
+    authDomain: config.public.authDomain as string,
+    projectId: config.public.projectId as string,
+    storageBucket: config.public.storageBucket as string,
+    messagingSenderId: config.public.messagingSenderId as string,
+    appId: config.public.appId as string,
+    measurementId: config.public.measurementId as string,
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  // nuxtApp.vueApp.provide("auth", auth);
+  // nuxtApp.provide("auth", auth);
+});
