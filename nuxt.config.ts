2b710229df154424b739aad6b92b155e4554b82c
Joseph Matthew Caballas <joseph.caballas@bld.ai>
2023-09-06 02:19:47 +0800

    feat: Add meta tags for Open Graph and Twitter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,6 +35,54 @@ export default defineNuxtConfig({
           content:
             "Add, edit, and delete your favorite food with Favorita. Keep track of your own gastronomic adventures.",
         },
+        {
+          property: "og:url",
+          content: "https://jmcaballas-favorita.vercel.app/",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:title",
+          content: "Favorita - Repository For Your Favorite Food",
+        },
+        {
+          property: "og:description",
+          content:
+            "Add, edit, and delete your favorite food with Favorita. Keep track of your own gastronomic adventures.",
+        },
+        {
+          property: "og:image",
+          content:
+            "https://firebasestorage.googleapis.com/v0/b/jmcaballas-favorita.appspot.com/o/dOWV16i2pzTAAkYC7kIaZ3L5PlD2%2F%5B1693919399570%5DDSC03451?alt=media&token=07b565cd-f459-47d5-bcc7-f0d98ec92d90",
+        },
+        {
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        {
+          property: "twitter:domain",
+          content: "jmcaballas-favorita.vercel.app",
+        },
+        {
+          property: "twitter:url",
+          content: "https://jmcaballas-favorita.vercel.app/",
+        },
+        {
+          name: "twitter:title",
+          content: "Favorita - Repository For Your Favorite Food",
+        },
+        {
+          name: "twitter:description",
+          content:
+            "Add, edit, and delete your favorite food with Favorita. Keep track of your own gastronomic adventures.",
+        },
+        {
+          name: "twitter:image",
+          content:
+            "https://firebasestorage.googleapis.com/v0/b/jmcaballas-favorita.appspot.com/o/dOWV16i2pzTAAkYC7kIaZ3L5PlD2%2F%5B1693919399570%5DDSC03451?alt=media&token=07b565cd-f459-47d5-bcc7-f0d98ec92d90",
+        },
       ],
       link: [
         {
